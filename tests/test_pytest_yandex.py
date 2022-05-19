70b5a3d7a74314ab540203238a24e4d131283f1c
Vernik-S <vernik.krd@gmail.com>
2022-05-20 01:40:32 +0300

    Negative test 409 added

--- tests/test_pytest_yandex.py
+++ tests/test_pytest_yandex.py
@@ -12,5 +12,6 @@ class TestYandex:
 
         test_dir_name = "Netology_test_dir"
         assert test_yandex.create_dir(test_dir_name) == 201
+        assert test_yandex.create_dir(test_dir_name) == 409 #отрицательный тест при попытке создания существующей папки
         assert "disk:/"+test_dir_name in test_yandex.get_dir_list()
         test_yandex.del_dir(test_dir_name)
